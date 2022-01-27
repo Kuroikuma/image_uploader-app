09e27038eec7e97607987cdbc1a35f079daf6296
OdiseoJR <odiseoj676@gmail.com>
2022-01-27 00:17:29 -0600

    feat(Root): start project

---
+++ src/app/page/page.jsx
@@ -0,0 +1,8 @@
+import style from './page.modele.css'
+export const Page = () => {
+  return (
+    <>
+      <h1 className={style.App}> Hola mundo</h1>
+    </>
+  )
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Page } from './app/page/page'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Page />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
