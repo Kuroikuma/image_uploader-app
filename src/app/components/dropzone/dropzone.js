e5fd61d7fe3ffa4dd056390654349199539876c6
Kuroikuma <odiseoj676@gmail.com>
2022-01-27 22:23:53 -0600

    feat(Root):añadiendo svg de fondo y implementado el custom hook en el componente dropzone

--- src/app/components/dropzone/dropzone.js
+++ src/app/components/dropzone/dropzone.js
@@ -1,12 +1,14 @@
 import React, { useCallback } from 'react'
 import { useDropzone } from 'react-dropzone'
+import { useFileStorage } from './useFileStorage'
 
 export function DropzoneFile () {
+  const [fileUrl, fileHandler] = useFileStorage()
   const onDrop = useCallback((acceptedFiles) => {
     acceptedFiles && acceptedFiles.map((file) => {
-      return console.log(file)
+      return fileHandler(file)
     })
-  }, [])
+  }, [fileHandler])
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
   return (
@@ -19,6 +21,7 @@ export function DropzoneFile () {
         : (
           <p>Drag 'n' drop some files here, or click to select files</p>
           )}
+      {fileUrl ? <img src={fileUrl} alt='FileStorage' /> : null}
     </div>
   )
 }
--- src/app/page/page.jsx
+++ src/app/page/page.jsx
@@ -1,8 +1,10 @@
 import style from './page.module.css'
+import { DropzoneFile } from '../components/dropzone/dropzone'
 export const Page = () => {
   return (
     <>
-      <h1 className={style.App}> Hola mundo</h1>
+      <h1 className={style.title}> Hola mundo</h1>
+      <DropzoneFile />
     </>
   )
 }
